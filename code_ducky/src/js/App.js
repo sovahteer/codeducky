37d251ca0c7df095be5e77f9f53f4f40a01a9fd9
Unknown <sebastiaan.sepers@gmail.com>
2018-11-07 12:59:29 +0100

    Added component files and basic Firebase initialization.

--- code_ducky/src/js/App.js
+++ code_ducky/src/js/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBtcD656OwghKMY_lRka6lws85y51XX5fs",
+      authDomain: "codeducky-f79ed.firebaseapp.com",
+      databaseURL: "https://codeducky-f79ed.firebaseio.com",
+      projectId: "codeducky-f79ed",
+      storageBucket: "codeducky-f79ed.appspot.com",
+      messagingSenderId: "761178755848"
+    };
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
       <div>
