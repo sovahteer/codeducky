2c4e9c0219c71df1114b369bd54828221eaaef39
Unknown <sebastiaan.sepers@gmail.com>
2018-11-08 16:25:58 +0100

    Updated firebase config stuff

--- code_ducky/src/js/App.js
+++ code_ducky/src/js/App.js
@@ -4,19 +4,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import Header from './components/Header.js';
 import Content from './components/Content.js';
+import config from './config.js';
 
 class App extends Component {
   constructor(props){
     super(props);
     // Initialize Firebase
-    const config = {
-      apiKey: "AIzaSyBtcD656OwghKMY_lRka6lws85y51XX5fs",
-      authDomain: "codeducky-f79ed.firebaseapp.com",
-      //databaseURL: "https://codeducky-f79ed.firebaseio.com",
-      projectId: "codeducky-f79ed",
-      //storageBucket: "codeducky-f79ed.appspot.com",
-      messagingSenderId: "761178755848"
-    };
     if(!firebase.apps.length)
       firebase.initializeApp(config);
     
